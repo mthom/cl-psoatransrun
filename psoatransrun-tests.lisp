70cf246cf1959ebad8068bdcb1e11d68c204b4c6
Mark Thom <markjordanthom@gmail.com>
2020-11-10 14:30:37 -0700

    add docstrings to psoatransrun-tests.lisp

--- psoatransrun-tests.lisp
+++ psoatransrun-tests.lisp
@@ -4,37 +4,53 @@
 (named-readtables:in-readtable rutils-readtable)
 
 (defparameter *test-suite-directory*
-  "/home/mark/git/PSOATransRunComponents/PSOATransRun/test/")
+  #p"/home/mark/git/PSOATransRunComponents/PSOATransRun/test/"
+  "The path to PSOATransRunComponents test suite. Change this to point
+  to your local copy!")
 
 (defun file-pathname (containing-dir-pathname filename)
+  "Conjoins \"filename\" to the directory \"pathname\" and returns the
+resulting pathname."
   (merge-pathnames containing-dir-pathname (pathname filename)))
 
 (defun file-get-contents (filename)
+  "Slurps the contents of a file to a string, which is returned."
   (with-open-file (stream filename)
     (let ((contents (make-string (file-length stream))))
       (read-sequence contents stream)
       contents)))
 
 (defun kb-pathname (subdirectory)
+  "The test suite KB name is always derived from the name of its containing subdirectory,
+appending \"-KB.psoa\" to produce a filename."
   (file-pathname subdirectory
                  (format nil "~A-KB.psoa" (directory-name subdirectory))))
 
 (defun query-pathname (subdirectory n)
+  "Query files are labeled with an integer \"n\" and count upwards."
   (file-pathname subdirectory
                  (format nil "~A-query~D.psoa"
                          (directory-name subdirectory) n)))
 
 (defun answer-pathname (subdirectory n)
+  "Answer files are labeled with an integer \"n\" and count upwards."
   (file-pathname subdirectory
                  (format nil "~A-answer~D.psoa"
                          (directory-name subdirectory) n)))
 
 (defun list-subdirectories (pathname)
+  "List the subdirectories of the directories at \"pathname\"."
   (remove-if-not #'directory-p
                  (directory (make-pathname :name :wild :type :wild
                                            :defaults pathname))))
 
 (defun equal-answer-set-p (answer-set-1 answer-set-2)
+  "Answer sets are are either strings or lists of PSOA RuleML AST nodes.
+To be equal, \"answer-set-1\" and \"answer-set-2\" must share one of
+these types. If they are both strings, they are checked for equality.
+If they are both lists of AST nodes, their are pretty printed to
+strings and checked for equality modulo list ordering via the function
+set-exclusive-or."
   (cond
     ((and (typep answer-set-1 'string) (typep answer-set-2 'string))
      (string= answer-set-1 answer-set-2))
@@ -46,10 +62,15 @@
      nil)))
 
 (defun answers-match-p (expected-answers reported-answers)
+  "Two answers set match iff all their corresponding answer sets match."
   (every #'equal-answer-set-p expected-answers reported-answers))
 
 (defun run-test-case (test-kb-filename subdirectory engine-client
                       prefix-ht relationships)
+  "Iterate through query and answer files, send them to the Prolog
+engine client, get back the answers, and compare them with the answers
+expected by the test case in the corresponding answer file. Print a
+message when a query fails."
   (let ((engine-socket (prolog-engine-client-socket engine-client))
         (*all-solutions* t))
     (loop for n upfrom 1
@@ -80,6 +101,11 @@
             (throw 'continue nil))))
 
 (defun run-test-suite (&key (system :scryer))
+  "Create and destroy a fresh instance of the Prolog engine derived
+from the \"system\" keyword for each test case in
+*test-suite-directory*. For now, print parse errors and other error
+types as notifications to the screen, but don't allow them to prevent
+further tests."
   (let ((subdirectories (list-subdirectories *test-suite-directory*))
         (engine-client (make-engine-client system)))
     (dolist (subdirectory subdirectories)
